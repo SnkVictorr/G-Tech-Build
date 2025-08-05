9ebcfe73e4616b626317f36257385e8194acd496
Br12zin <Brunolobo2110@gmail.com>
2025-08-05 18:53:30 -0300

    feat(atualizado): novo

---
+++ src/app/gerenciador.tsx
@@ -0,0 +1,24 @@
+import {View } from 'react-native'
+import React from 'react'
+import GerenciadorEquipamentos from '../components/Gerenciador'
+import { Header } from '../components/header'
+import { StyleSheet } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
+
+export default function Gerenciamento() {
+    return (
+        <View style={styles.container}>
+            <Header texto='Localizar Equipamentos' local='/tela-inicial'
+                icon={<AntDesign name="back" size={24} color="white" />} />
+            <GerenciadorEquipamentos />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create ({
+    container: {
+        paddingBottom: 30,
+        backgroundColor: "#ececec",
+        flex: 1
+    },
+})
